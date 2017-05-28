7eda8e02223cf48ffc9b2a7b2db638b5db6d085c
Shimon Shrem <s.shrem@f5.com>
2017-05-28 13:20:08 +0300

    facebook share url links

--- scripts/createDesigns.py
+++ scripts/createDesigns.py
@@ -8,18 +8,24 @@ bedroomOffering=[5,6,13,14]
 bathroomOffering=[10,11,12,15]
 kitchenOffering=[21,22,23,24]
 roomIds=[2,1,3,4]
-facebookUrl='https://www.facebook.com/1848414988734528/videos/1848855945357099/'
+facebookUrl=[
+			'https://www.facebook.com/1848414988734528/videos/1896675820575111/',
+			'https://www.facebook.com/1848414988734528/videos/1896677050574988/',
+			'https://www.facebook.com/1848414988734528/videos/1896678040574889/',
+			'https://www.facebook.com/1848414988734528/videos/1896678943908132/'
+			]
 tab='\t'
 fileName = "/Users/shrem/dev/ds/designs.tsv"
 f = open(fileName, 'w')
 
 f.write('ProjectId\tAptTmplCode\tDesignTitle\tDesignerId\tDesignImgCode\tOffering\tRoom\n')
-for apt in aptCode:
+
+for aptIdx, apt in enumerate(aptCode):
 	for lRoom in livingRoomOffering:
 		for bRoom in bedroomOffering:
 			for bathRoom in bathroomOffering:
 				for kRoom in kitchenOffering:
-					f.write('{}\t{}\t{}\t{}\t{}\t{}\t2\t{}\n'.format(projectId, apt, designTitle, designerId, designImgCode, lRoom, facebookUrl))
-					f.write('\t\t\t\t\t{}\t1\t{}\n'.format(bRoom, facebookUrl))
-					f.write('\t\t\t\t\t{}\t3\t{}\n'.format(bathRoom, facebookUrl))
-					f.write('\t\t\t\t\t{}\t4\t{}\n'.format(kRoom, facebookUrl))
+					f.write('{}\t{}\t{}\t{}\t{}\t{}\t2\t{}\n'.format(projectId, apt, designTitle, designerId, designImgCode, lRoom, facebookUrl[aptIdx]))
+					f.write('\t\t\t\t\t{}\t1\t{}\n'.format(bRoom, facebookUrl[aptIdx]))
+					f.write('\t\t\t\t\t{}\t3\t{}\n'.format(bathRoom, facebookUrl[aptIdx]))
+					f.write('\t\t\t\t\t{}\t4\t{}\n'.format(kRoom, facebookUrl[aptIdx]))
